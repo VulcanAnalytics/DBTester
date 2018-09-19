bfe3b426b5cc6bbbd796750cf74c997b622c4359
Edward Comeau <edward.comeau@vulcananalytics.co.uk>
2018-09-19 09:37:30 +0100

    Refactored as a .net 461 project and added simplist of functionality to prove build pipeline

---
+++ src/dbSpecflow_net461/DatabaseTester.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Data.SqlClient;
+using VulcanAnalytics.DBTester.Exceptions;
+
+namespace VulcanAnalytics.DBTester
+{
+    public class DatabaseTester
+    {
+        private string defaultSchema;
+
+        private SqlConnection connection = new SqlConnection();
+
+        private Database database;
+
+        public DatabaseTester(string connectionString)
+        {
+            defaultSchema = "dbo";
+            Server dbServer;
+
+            try
+            {
+                connection.ConnectionString = connectionString;
+
+                dbServer = new Server(connectionString);
+                var c = dbServer.Databases.Count;
+            }
+            catch (Exception e)
+            {
+                var newerror = new DatabaseTesterConnectionException("There was a problem with your connection string", e);
+                throw newerror;
+            }
+
+            var connectionDatabase = connection.Database;
+
+            this.database = dbServer.Databases[connectionDatabase];
+        }
+
+        public bool HasSchema(string schemaName)
+        {
+            return database.Schemas.Contains(schemaName);
+        }
+
+        public bool HasTable(string tableName)
+        {
+            return HasTable(tableName, defaultSchema);
+        }
+
+        public bool HasTable(string schemaName, string tableName)
+        {
+            return database.Tables.Contains(tableName, schemaName);
+        }
+    }
+}
---
+++ tests/dbSpecflow_tests/DatabaseTesterTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VulcanAnalytics.DBTester.Exceptions;
+
+namespace VulcanAnalytics.DBTester.dbSpecflow_tests
+{
+    [TestClass]
+    public class DatabaseTesterTests
+    {
+        const string unavailableConnString = @"Data Source=(localdb)\blah;Initial Catalog=foobar;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
+        const string availableConnString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=tempdb;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True";
+
+        private Type databasetesterType = typeof(DatabaseTester);
+
+        [TestMethod]
+        public void DatabaseTesterClass()
+        {
+            VulcanAnalytics.DBTester.DatabaseTester tester = null;
+        }
+
+        [TestMethod]
+        public void DatabaseTesterConstructorString()
+        {
+            try
+            {
+                var tester = new DatabaseTester(availableConnString);
+            }
+            catch { }
+        }
+
+        [TestMethod]
+        public void DatabaseTesterConstructorRejectsInvalidConnectionString()
+        {
+            try
+            {
+                var tester = new DatabaseTester("not a connection string");
+
+                Assert.Fail();
+            }
+            catch (AssertFailedException)
+            {
+                Assert.Fail("Invalid connection string accepted");
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DatabaseTesterConnectionException));
+            }
+        }
+
+        [TestMethod]
+        public void DatabaseTesterConstructorRejectsUnavailableConnection()
+        {
+            try
+            {
+                var tester = new DatabaseTester(unavailableConnString);
+
+                Assert.Fail();
+            }
+            catch (AssertFailedException)
+            {
+                Assert.Fail("Unavailable connection string accepted");
+            }
+            catch (Exception e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DatabaseTesterConnectionException));
+            }
+        }
+
+        [TestMethod]
+        public void DatabasetesterHasHasTableMethod()
+        {
+            var methodFound = false;
+
+            var methods = GetMethods(databasetesterType, "HasTable");
+            if (methods.Length > 0)
+            {
+                methodFound = true;
+            }
+
+            Assert.IsTrue(methodFound, "Method of required name not found");
+        }
+
+        [TestMethod]
+        public void DatabaseTesterMethodHasTableReturnsBool()
+        {
+            Type returnType = null;
+
+            var methods = GetMethods(databasetesterType, "HasTable");
+            if (methods.Length > 0)
+            {
+                returnType = methods[0].ReturnType;
+            }
+
+            Assert.AreEqual(typeof(bool), returnType,"HasTable method doesn't return the correct type");
+        }
+
+        private MethodInfo[] GetMethods(Type type,string name)
+        {
+            List<MethodInfo> methods = new List<MethodInfo>();
+
+
+            foreach (var method in type.GetMethods())
+            {
+                if (method.Name == name)
+                {
+                    methods.Add(method);
+                }
+            }
+
+            return methods.ToArray();
+
+        }
+    }
+}
