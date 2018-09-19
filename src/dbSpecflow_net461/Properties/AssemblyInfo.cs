fe893ebd6c7300be930d9c8b561b37588d598b65
Edward Comeau <edward.comeau@vulcananalytics.co.uk>
2018-09-20 00:08:09 +0100

    Corrected assembly names to VulcanAnalytics.DBTester
    Release updated;
    Version 0.1.0-alpha
    Build 0006

--- src/dbSpecflow_net461/Properties/AssemblyInfo.cs
+++ src/dbSpecflow_net461/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DBTester")]
+[assembly: AssemblyTitle("VulcanAnalytics.DBTester")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vulcan Analytics Limited")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0004")]
-[assembly: AssemblyFileVersion("0.1.0.0004")]
+[assembly: AssemblyVersion("0.1.0.0006")]
+[assembly: AssemblyFileVersion("0.1.0.0006")]
