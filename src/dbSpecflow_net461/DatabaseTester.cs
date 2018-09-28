0e64fb253b7953d8213c3d0a828393a6ed727df8
Darren Comeau <darren.comeau@vulcananalytics.co.uk>
2018-09-28 14:32:25 +0100

    refactored InsertData method

--- src/dbSpecflow_net461/DatabaseTester.cs
+++ src/dbSpecflow_net461/DatabaseTester.cs
@@ -44,6 +44,7 @@ namespace VulcanAnalytics.DBTester
             this.database = dbServer.Databases[connectionDatabase];
         }
 
+
         public bool HasSchema(string schemaName)
         {
             return database.Schemas.Contains(schemaName);
@@ -77,35 +78,66 @@ namespace VulcanAnalytics.DBTester
 
         public void InsertData(string schemaName, string objectName, string[] columns, Object[] data)
         {
-            string sqlTemplate = "insert into {0}.{1}({2}) values({3});";
-
-            string sqlColumns = string.Empty;
-            foreach (var s in columns)
-            {
-                sqlColumns += string.Format("{0},",s);
-            }
-            sqlColumns = sqlColumns.TrimEnd(',');
+            string sqlColumns = SqlColumns(columns);
 
             foreach (Object[] row in data)
             {
-                string sqlValues = string.Empty;
-                foreach (var s in row)
-                {
-                    sqlValues += string.Format("'{0}',", s);
-                }
-                sqlValues = sqlValues.TrimEnd(',');
+                string sqlValues = SqlValues(row);
 
-                var sql = string.Format(sqlTemplate, schemaName, objectName, sqlColumns, sqlValues);
+                var sql = SqlInsertStatement(schemaName, objectName, sqlColumns, sqlValues);
 
-                try
-                {
-                    this.ExecuteStatementWithoutResult(sql);
-                }
-                catch (Exception e)
-                {
-                    throw new Exception(String.Format("Error encountered executing the insert statement: {0}",sql), e);
-                }
+                TryToInsertRow(sql);
             }
+        }
+
+
+        private void TryToInsertRow(string insertStatement)
+        {
+            try
+            {
+                this.ExecuteStatementWithoutResult(insertStatement);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(String.Format("Error encountered executing the insert statement: {0}", insertStatement), exception);
+            }
+        }
+
+        private string SqlColumns(Object[] columns)
+        {
+            string sqlColumns = ArrayAsTemplatedString(columns, "{0}", ",");
+
+            return sqlColumns;
+        }
+
+        private string SqlValues(Object[] values)
+        {
+            string sqlValues = ArrayAsTemplatedString(values, "'{0}'",",");
+
+            return sqlValues;
+        }
+
+        private string ArrayAsTemplatedString(Object[] array, string template, string seperator)
+        {
+            string arrayString = string.Empty;
+
+            foreach (var arrayItem in array)
+            {
+                arrayString += string.Format(template, arrayItem) + seperator;
+            }
+
+            arrayString = arrayString.TrimEnd(seperator.ToCharArray());
+
+            return arrayString;
+        }
+
+        private string SqlInsertStatement(string schemaName, string objectName, string sqlColumns, string sqlValues)
+        {
+            string template = "insert into {0}.{1}({2}) values({3});";
+
+            var statement = string.Format(template, schemaName, objectName, sqlColumns, sqlValues);
+
+            return statement;
         }
     }
 }
