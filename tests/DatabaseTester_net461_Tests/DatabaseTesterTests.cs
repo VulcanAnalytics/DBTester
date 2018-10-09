3ad2dbefa35f3f90ea86946de13848ec0067e968
Darren Comeau <darren.comeau@vulcananalytics.co.uk>
2018-10-09 22:26:00 +0100

    InsertData with NULLs example

--- tests/DatabaseTester_net461_Tests/DatabaseTesterTests.cs
+++ tests/DatabaseTester_net461_Tests/DatabaseTesterTests.cs
@@ -149,6 +149,35 @@ namespace VulcanAnalytics.DBTester.dbSpecflow_tests
             tester.InsertData(schemaName, tableName, columns, data, defaults);
         }
 
+        [TestMethod]
+        public void InsertDataWithNullsWikiExample()
+        {
+            // Setup the Database Tester object using the MS SQL implementation
+            var connectionstring = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=tempdb;Integrated Security=SSPI;";
+            VulcanAnalytics.DBTester.DatabaseTester tester = new VulcanAnalytics.DBTester.MsSqlDatabaseTester(connectionstring);
+
+            // Create a table to insert data into
+            tester.ExecuteStatementWithoutResult("drop table if exists [dbo].[TestPerson];");
+            tester.ExecuteStatementWithoutResult("create table [dbo].[TestPerson]([TestId] int, [Name] varchar(255), [ModifiedDate] datetime, [ModifiedBy] sysname);");
+
+            // Set the defaults for the insert
+            var defaults = new ColumnDefaults();
+            defaults.AddDefault(new KeyValuePair<string, object>("ModifiedBy", "DarrenComeau"));
+
+            // Parameters for InsertData method
+            var schemaName = "dbo";
+            var tableName = "TestPerson";
+            var columns = new string[] { "TestId", "Name", "ModifiedDate" };
+            var data = new object[]
+                {
+                    new Object[]{1,"Joe","2018-09-28T12:49:13.576"},
+                    new Object[]{2,null,"2018-10-01T18:31:29.256"}
+                };
+
+            // Call the Insertdata method to insert the rows
+            tester.InsertData(schemaName, tableName, columns, data, defaults);
+        }
+
         #region Private Methods
         private bool HasMethod(Type type, string methodName)
         {
